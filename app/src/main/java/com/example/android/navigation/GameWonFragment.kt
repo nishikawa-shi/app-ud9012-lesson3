a92e83dffb7752d52daf18aea9deaf3c32c13677
Tetsuya Nishikawa <tetphi1dev@gmail.com>
2019-01-18 00:32:15 +0900

    Navigation内の遷移のIDを指定する際の記述の抽象度を高めた

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -34,7 +34,7 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
         //TODO: 以下の強制的アンラップをなんとかしたい
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
